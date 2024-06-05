12409910c62b4643beecf571b6a95c3cebd0aa7e
lalit-machra <lalitmachra0403@gmail.com>
2024-06-05 22:18:14 +0530

    Get computer choice

---
+++ script.js
@@ -0,0 +1,19 @@
+function getComputerChoice() {
+  // Generate a random number between 1 and 100
+  let randomNum = Math.floor(Math.random() * 100) + 1;
+  let computerChoice;
+
+  // Convert randomNum to Rock, Paper and Scissors
+  if (randomNum > 0 && randomNum <= 33) {
+    computerChoice = "Rock";
+  }
+  else if (randomNum > 33 && randomNum <= 66) {
+    computerChoice = "Paper";
+  }
+  else {
+    computerChoice = "Scissors";
+  }
+
+  return computerChoice;
+}
+
